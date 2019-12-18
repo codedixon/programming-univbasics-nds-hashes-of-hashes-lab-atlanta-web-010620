289342697d773f5238c8e588dca835fad1026167
Rashad Dixon <dixoncodes@gmail.com>
2019-12-18 18:33:02 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,10 +32,25 @@
 
 def naming_system
 PORTION_1 = {
-  Label: "Kingdom",
-  
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+    }
+  }
 }
-PORTION_2 = {}
-PORTION_3 = {}
-PORTION_4 = {}
+PORTION_2 = {
+  label: "Order"
+}
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus"
+  }
+}
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
 end
