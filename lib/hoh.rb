c3e29397e18224ecc589983492b50f02d09e1308
Rashad Dixon <dixoncodes@gmail.com>
2019-12-18 19:05:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,13 +33,13 @@
   
 def naming_system
   starting_node = {
-    label: "Kingdom"
-    sub_category: {
-      label: "Phylum"
+    :label => "Kingdom",
+    :sub_category => {
+      :label => "Phylum",
       :sub_category {
-        label: "Class"
-        sub_category = {
-          label: "Order"
+        :label => "Class",
+        :sub_category => {
+          :label => "Order"
         }
       }
     }
