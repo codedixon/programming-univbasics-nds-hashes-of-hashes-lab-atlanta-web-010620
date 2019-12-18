12d24cadb56f1d3c88eef3864d90190c8e33fef6
Rashad Dixon <dixoncodes@gmail.com>
2019-12-18 18:37:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,24 +41,24 @@ PORTION_1 = {
     }
   }
 }
-PORTION_2 = {
-  label: "Order"
-}
-PORTION_3 = {
-  label: "Family",
-  sub_category: {
-    label: "Genus"
-  }
-}
-PORTION_4 = {
+#PORTION_2 = {
+ # label: "Order"
+#}
+#PORTION_3 = {
+ # label: "Family",
+  #sub_category: {
+   # label: "Genus"
+  #}
+#}
+#PORTION_4 = {
   label: "Species",
   sub_category: nil
-}
+#}
 HoH = {
   PORTION_1,
-  PORTION_2,
-  PORTION_3,
-  PORTION_4
+  #PORTION_2,
+  #PORTION_3,
+ # PORTION_4
 }
 return HoH
 end
