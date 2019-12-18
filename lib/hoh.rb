ef2627ef3a8d0e318888145f9bd2aa82fda65565
Rashad Dixon <dixoncodes@gmail.com>
2019-12-18 19:12:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,7 +42,7 @@ def naming_system
           :label => "Order",
           :sub_category => {
             :label => "Family",
-            :sub_category => {
+            sub_category {
               :label => "Genus",
               :sub_category => {
                 :label => "Species",
